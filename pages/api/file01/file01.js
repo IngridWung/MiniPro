5db736d3de40df34549e5cc4c79b29a6b56b641e
IngridWung <ingridwungwork@gmail.com>
2023-11-27 15:03:50 +0800

    展示图片已部署到云端，解决了页面内容高于页面导致的渐变背景断层问题。

--- pages/api/file01/file01.js
+++ pages/api/file01/file01.js
@@ -12,11 +12,16 @@ Page({
       sizeType:['original', 'compressed'],// 指是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 指定来源是相册还是相机，默认二者都有
       success(res) {
-      	let file = res.tempFilePaths[0]
+       let file = res.tempFilePaths[0]
+        // const fileName = res.tempFilePaths[0].slice(11)/////new
+        
         wx.uploadFile({  
 	      url: 'https://www.bing.com/',  //随便写的网址
-	      name: 'image',//根据后端接口开发文档的要求填写
+//        cloudPath:'pics/'+fileName+'.png',//////new
 	      filePath: file,
+//        filePath:res.tempFilePaths,///////new
+	      name: 'image',//根据后端接口开发文档的要求填写
+
 	      success: function(result){  
           console.log(result);
           console.log("success upload");
@@ -30,7 +35,9 @@ Page({
   },
   downloadF(){
     wx.downloadFile({
-      url: 'https://example.com/audio/123', //随便的
+    url: 'https://example.com/audio/123', //随便的
+    //  url:'cloud://mainprogram-1g34f814e053b220.6d61-mainprogram-1g34f814e053b220-1322214062/pics/town_in_snow.jpg',
+      // cloudPath:"cloud://mainprogram-1g34f814e053b220.6d61-mainprogram-1g34f814e053b220-1322214062/pics/town_in_snow.jpg",
       success (res) {
         // 服务器响应数据，就会把响应内容写入文件并进入 success 回调，需要自行判断是否下载到了想要的内容
         if (res.statusCode === 200) {
